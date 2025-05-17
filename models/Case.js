285eba0683775117815ad0591811ace5738ee6d1
sekhar-jpg <bhanuhomeohospital@gmail.com>
2025-05-17 22:51:55 +0530

    Update Case.js

--- models/Case.js
+++ models/Case.js
@@ -1,74 +1,30 @@
 const mongoose = require("mongoose");
 
+const followUpSchema = new mongoose.Schema({
+  date: String,
+  notes: String,
+});
+
 const caseSchema = new mongoose.Schema({
-  patientInfo: {
-    name: String,
-    age: String,
-    gender: String,
-    phone: String,
-    address: String,
-    email: String,
-  },
-  chiefComplaints: {
-    mainComplaint: String,
-    duration: String,
-    severity: String,
-    aggravatingFactors: String,
-    relievingFactors: String,
-  },
-  clinicalDiagnosis: {
-    primaryDiagnosis: String,
-    diagnosisNotes: String,
-  },
-  doctorObservations: {
-    generalAppearance: String,
-    physicalFindings: String,
-    emotionalState: String,
-    otherObservations: String,
-  },
-  mentalGenerals: {
-    mindState: String,
-    fearAnxiety: String,
-    sleepPatterns: String,
-  },
-  familyHistory: {
-    familyDiseases: String,
-    hereditaryConditions: String,
-    mentalIllnessHistory: String,
-    otherHistory: String,
-  },
-  personalHistory: {
-    habits: String,
-    occupation: String,
-    sleepPattern: String,
-    appetite: String,
-    thirst: String,
-  },
-  pastHistory: {
-    pastIllnesses: String,
-    pastSurgeries: String,
-    allergies: String,
-    hospitalizations: String,
-  },
-  miasmaticDiagnosis: {
-    psora: String,
-    sycosis: String,
-    syphilis: String,
-    tubercular: String,
-  },
-  prescriptionDetails: {
-    medicine: String,
-    dosage: String,
-    frequency: String,
-  },
-  faceImage: {
-    type: String,
-    default: null,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+  patientName: String,
+  age: String,
+  gender: String,
+  phone: String,
+  address: String,
+  chiefComplaints: String,
+  history: String,
+  mind: String,
+  physical: String,
+  generals: String,
+  familyHistory: String,
+  investigations: String,
+  diagnosis: String,
+  miasm: String,
+  constitution: String,
+  prescription: String,
+  date: String,
+  faceImage: Buffer,
+  followUps: [followUpSchema],
 });
 
 module.exports = mongoose.model("Case", caseSchema);
